a049095b316ee173761028af8947dc68699c69da
kirillkrohmal <krohmal_kirill@mail.ru>
2020-05-25 14:55:20 +0300

    junior

--- src/main/java/com/urise/webapp/model/Resume.java
+++ src/main/java/com/urise/webapp/model/Resume.java
@@ -1,17 +1,18 @@
 package com.urise.webapp.model;
 
+import java.util.Objects;
+
 /**
  * com.urise.webapp.model.Resume class
  */
-public class Resume {
+public class Resume implements Comparable<Resume>{
     public String uuid;
+
+    public Resume() {
+    }
 
     public Resume(String uuid) {
         this.uuid = uuid;
-    }
-
-    public Resume() {
-
     }
 
     public String getUuid(){
@@ -19,7 +20,25 @@ public class Resume {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(uuid, resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+
+    @Override
     public String toString() {
         return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
--- src/main/java/com/urise/webapp/run/MainArray.java
+++ src/main/java/com/urise/webapp/run/MainArray.java
@@ -18,7 +18,7 @@ public class MainArray {
         Resume r;
 
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | update | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | update uuid | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
